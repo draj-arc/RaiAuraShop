dd38679e963b3116f2a7d4bd8de3f1939b3315cc
RaiAura Shop <raiaura.shop@gmail.com>
2025-12-21 12:02:37 +0530

    Add Vercel configuration and CORS for rai-aura-shop.vercel.app

--- server/index.ts
+++ server/index.ts
@@ -14,6 +14,7 @@ app.use(cors({
     "https://raiaura.in",
     "https://www.raiaura.in",
     "https://raiaurashop.netlify.app",
+    "https://rai-aura-shop.vercel.app",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
